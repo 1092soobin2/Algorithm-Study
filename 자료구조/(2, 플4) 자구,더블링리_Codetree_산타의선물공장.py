26e04493ad59d8540b237a7131769875853069f9
1092soobin2 <1092soobin2@gmail.com>
2023-07-09 12:13:30 +0900

    double linked list (prv, nxt 로 관리하기)

--- 자료구조/(2, 플4) 자구,더블링리_Codetree_산타의선물공장.py
+++ 자료구조/(2, 플4) 자구,더블링리_Codetree_산타의선물공장.py
@@ -1,4 +1,4 @@
-# (1, 플4) Codetree_산타의선물공장 15 + 45~
+# (2, 플4) Codetree_산타의선물공장
 
 
 # === input ===
@@ -13,45 +13,61 @@ BROKEN, HEAD, TAIL = 0, 1, 2
 WEIGHT, B_NUM, PRV, NXT = 0, 1, 2, 3
 
 
+# 뒤에 붙이기
+def push(b_num, stuff_id):
+    global g_belt_list, g_stuff_dict
+
+    g_stuff_dict[stuff_id][B_NUM] = b_num
+
+    if g_belt_list[b_num][HEAD] == 0:
+        g_belt_list[b_num][HEAD] = stuff_id
+        g_belt_list[b_num][TAIL] = stuff_id
+    else:
+        tail_id = g_belt_list[b_num][TAIL]
+        g_stuff_dict[tail_id][NXT] = stuff_id
+        g_stuff_dict[stuff_id][PRV] = tail_id
+        g_belt_list[b_num][TAIL] = stuff_id
+
+
+def popleft(b_num):
+    global g_belt_list, g_stuff_dict
+
+    head_id = g_belt_list[b_num][HEAD]
+    if head_id == 0:
+        return 0
+    head_stuff = g_stuff_dict[head_id]
+
+    if head_stuff[NXT] == 0:
+        g_belt_list[b_num][HEAD] = 0
+        g_belt_list[b_num][TAIL] = 0
+    else:
+        g_stuff_dict[head_stuff[NXT]][PRV] = 0
+        g_belt_list[b_num][HEAD] = head_stuff[NXT]
+        head_stuff[NXT] = 0
+
+    return head_id
+
+
 # 1. 공장 설립
 def establish(info):
     global g_belt_list, g_stuff_dict
 
     # m개 밸트에 n개 물건
     n, m = info[0], info[1]
-    g_belt_list = [[False, 0, 0] for _ in range(m + 1)]
+    g_belt_list = [[True, 0, 0]] + [[False, 0, 0] for _ in range(m)]
 
     # 물건 [id, w] -> dict
     for b_num in range(1, m+1):
         b_idx = 2 + (n // m) * (b_num - 1)
 
-        # 벨트 리스트
-        g_belt_list[b_num][HEAD] = info[b_idx]
-        g_belt_list[b_num][TAIL] = info[b_idx + n // m - 1]
-
         # 선물 딕셔너리
-        g_stuff_dict[info[b_idx]] = [info[b_idx + n], b_num, 0, info[b_idx + 1]]
-        for idx in range(1, n // m - 1):
+        for idx in range(n // m):
             stuff_idx = b_idx + idx
             w_idx = stuff_idx + n
-            g_stuff_dict[info[stuff_idx]] = [info[w_idx], b_num, info[stuff_idx - 1], info[stuff_idx + 1]]
-
-        last_idx = b_idx + n // m - 1
-        g_stuff_dict[info[last_idx]] = [info[last_idx + n], b_num, info[last_idx - 1], 0]
-
+            g_stuff_dict[info[stuff_idx]] = [info[w_idx], b_idx, 0, 0]
+            push(b_num, info[stuff_idx])
     print_debug("establish() done.")
 
-
-# def pop(stuff_id):
-#     global g_stuff_dict
-#
-#     stuff = g_stuff_dict[stuff_id]
-#
-#     # head이면
-#     if stuff[PRV] == 0:
-#         s
-#
-# def push():
 
 # 2. 물건 하차
 # return : 하자된 물건 무게 총합
@@ -60,30 +76,24 @@ def get_off_stuff(w_max):
 
     total_w = 0
 
-    for belt_info in g_belt_list:
+    for b_num, belt_info in enumerate(g_belt_list):
         if belt_info[HEAD] == 0:
             continue
 
-        head_id = belt_info[HEAD]
-        head_stuff = g_stuff_dict[head_id]
-
         # 맨 앞에서 제거
-        belt_info[HEAD] = head_stuff[NXT]
-        g_stuff_dict[head_stuff[NXT]][PRV] = 0
-        head_stuff[NXT] = 0
+        head_id = popleft(b_num)
+        head_stuff = g_stuff_dict[head_id]
 
         # 각 벨트의 (맨 앞 선물 w) <= w_max -> 하차
         if head_stuff[WEIGHT] <= w_max:
-            del g_stuff_dict[head_id]
             total_w += head_stuff[WEIGHT]
+            del g_stuff_dict[head_id]
         # 그렇지 않으면 맨 뒤로 보냄
         else:
-            g_stuff_dict[belt_info[TAIL]][NXT] = head_id
-            head_stuff[PRV] = belt_info[TAIL]
-            belt_info[TAIL] = head_id
+            push(b_num, head_id)
 
     print_debug(f"200: get_off({w_max}) done.")
-    
+
     return total_w
 
 
@@ -144,8 +154,13 @@ def access_stuff(f_id):
         g_belt_list[stuff[B_NUM]][HEAD] = f_id
         stuff[PRV] = 0
     else:
-        tail_id = g_belt_list[stuff[B_NUM]][TAIL]
-        head_id = g_belt_list[stuff[B_NUM]][HEAD]
+        belt_info = g_belt_list[stuff[B_NUM]]
+        tail_id = belt_info[TAIL]
+        head_id = belt_info[HEAD]
+
+        # tail 갱신
+        belt_info[TAIL] = stuff[PRV]
+        g_stuff_dict[stuff[PRV]][NXT] = 0
 
         # head 갱신 (모두 앞으로 가져오기)
         g_stuff_dict[head_id][PRV] = tail_id
@@ -154,10 +169,6 @@ def access_stuff(f_id):
         g_belt_list[stuff[B_NUM]][HEAD] = f_id
         stuff[PRV] = 0
 
-        # tail 갱신
-        g_belt_list[stuff[B_NUM]][TAIL] = stuff[PRV]
-        g_stuff_dict[stuff[PRV]][NXT] = 0
-
     print_debug(f"400 access_stuff({f_id})")
     return g_stuff_dict[f_id][B_NUM]
 
@@ -165,6 +176,13 @@ def access_stuff(f_id):
 # 5. 벨트 고장
 def break_belt(b_num):
     global g_belt_list, g_stuff_dict
+
+    def move_all():
+        head_id = popleft(b_num)
+        nxt_b_num = g_belt_list.index(belt_info)
+        while head_id != 0:
+            push(nxt_b_num, head_id)
+            head_id = popleft(b_num)
 
     # 이미 망가져 있었다면 -1
     if g_belt_list[b_num][BROKEN]:
@@ -176,17 +194,17 @@ def break_belt(b_num):
         # 오른쪽 벨트부터, 고장 X 최초 벨트로 모든 물건 옮김
         for belt_info in g_belt_list[b_num:]:
             if not belt_info[BROKEN]:
-                tail_id = belt_info[TAIL]
-                head_id = g_belt_list[b_num][HEAD]
+                move_all()
+                print_debug(f"500 : break_belt({b_num})")
+                return b_num
 
-                g_stuff_dict[tail_id][NXT] = head_id
-                g_stuff_dict[head_id][PRV] = tail_id
+        for belt_info in g_belt_list[:b_num]:
 
-                belt_info[TAIL] = g_belt_list[b_num][TAIL]
+            if not belt_info[BROKEN]:
+                move_all()
+                print_debug("500")
+                return b_num
 
-    print_debug("500")
-
-    return b_num
 
 # 정상 처리 -> b_num
 
@@ -215,6 +233,8 @@ def print_debug(title=""):
 
     print("====================================")
     print(title)
+    for stuff_id in g_stuff_dict:
+        print(f"{stuff_id} : {g_stuff_dict[stuff_id]}")
     for belt_info in g_belt_list:
         print(belt_info[BROKEN], end="\t")
         curr_id = belt_info[HEAD]
