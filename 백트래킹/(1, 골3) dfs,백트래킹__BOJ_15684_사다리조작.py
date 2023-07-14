4689fc4193eb0584bf6dfb97c2872d6de4a273ad
1092soobin2 <1092soobin2@gmail.com>
2023-07-14 22:03:19 +0900

    백트래킹, 1340->148

--- 백트래킹/(1, 골3) dfs,백트래킹__BOJ_15684_사다리조작.py
+++ 백트래킹/(1, 골3) dfs,백트래킹__BOJ_15684_사다리조작.py
@@ -19,16 +19,25 @@ horizon_list = [list(map(int, input().split())) for _ in range(M)]
 # [가로idx][세로idx] = (연결선 idx) (0이면 연결 X)
 # 0번 row, 0 col -> dummy
 link_arr = [[0] * (N + 1) for _ in range(H + 1)]
+down_arr = [[0] * (N + 1) for _ in range(H + 1)]
 
 
 def init():
     global link_arr
 
-    link_arr[0] = [1] * (N + 1) # 첫 줄은 N/A
+    link_arr[0] = [1] * (N + 1)     # 첫 줄은 N/A
 
     for a, b in horizon_list:
         link_arr[a][b] = b + 1
         link_arr[a][b + 1] = b
+
+    for c in range(N, 0, -1):
+        down_r = H
+        for r in range(H, 0, -1):
+            if link_arr[r][c] == 0:
+                down_arr[r][c] = down_r
+            else:
+                down_r = r
 
 
 # 사다리 타기 O(N*H) 10 * 300 = 3000
@@ -40,7 +49,6 @@ def go_ladder(column) -> int:
 
 
 def check_all_ladder():
-
     for column in range(1, N + 1):
         if not go_ladder(column) == column:
             return False
@@ -48,24 +56,31 @@ def check_all_ladder():
 
 
 # 모든 경우 O(N*H) 10 * 300 = 3000
-def dfs(acc):
+def dfs(acc, ans):
     global link_arr
 
-    if acc > 3:
-        return -1
+    if acc >= ans:
+        return ans
 
     if check_all_ladder():
         return acc
-
+    
+    if acc == 3:
+        return -1
+    
     min_res = 4
     for r in range(1, H + 1):
         for c in range(1, N):
             # 위 연결이 있는 연결의 경우
             if (link_arr[r][c] == 0 and link_arr[r][c + 1] == 0) and not (link_arr[r - 1][c] == 0 and link_arr[r - 1][c + 1] == 0):
                 link_arr[r][c], link_arr[r][c+1] = c+1, c
-                res = dfs(acc + 1)
-                min_res = min(res, min_res) if res != -1 else min_res
-                link_arr[r][c], link_arr[r][c+1] = 0, 0
+                res = dfs(acc + 1, min_res)
+                link_arr[r][c], link_arr[r][c + 1] = 0, 0
+                
+                if res == acc + 1:
+                    return res
+                elif res != -1:
+                    min_res = min(res, min_res)
 
     return min_res if min_res != 4 else -1
 
@@ -73,7 +88,7 @@ def dfs(acc):
 def solution():
 
     init()
-    print(dfs(0))
+    print(dfs(0, 4))
 
 
 # === output ===
