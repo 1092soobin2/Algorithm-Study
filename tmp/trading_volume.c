b848666ce6140d57712dc9b9be7c0ab19f5b7698
1092soobin2 <1092soobin2@gmail.com>
2023-08-03 23:35:23 +0900

    c 구조체, memcp 등

---
+++ tmp/trading_volume.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// header file include
+
+
+
+// ㅊㄱ 포트만 적기 ([TODO: 61 수정, )
+int socket[61] = {0, };
+char ip_group[61][15] = {"0.0.0.0",};
+int port_group[61] = {0, };
+
+int main(void) {
+
+	char buff[1024];
+
+	/* custom */
+	struct StructName variable_name;
+
+	char stock_code = buff[24];
+}
